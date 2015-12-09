aa3093ad9cbdca0b818a0dc1128a1c523860989b
Le Quang Thanh <thanh@robotbase.com>
2015-12-10 03:03:55 +0700

    add decrtoption of lib

--- Libraries/mylib/MyAdc.h
+++ Libraries/mylib/MyAdc.h
@@ -6,4 +6,9 @@
 #include "stm32f10x_adc.h"
 #include "stm32f10x_dma.h"
 void initAdc(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin,__IO uint16_t* ConvertedValue);
+/*
+GPIOx: Port chua chan can khoi tao chuc nang ADC
+GPIO_Pin: Chan Can Khoi Tao ADC
+ConvertedValue: Bien con tro de luu gia tri tu dong connvert
+*/
 #endif
